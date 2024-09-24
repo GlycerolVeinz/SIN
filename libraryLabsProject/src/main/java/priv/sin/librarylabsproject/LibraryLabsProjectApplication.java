9dcaece4d68c22d57b17928899d60003a4dd6e01
GlycerolVeinz <safrankou.matvei@gmail.com>
2024-09-24 19:19:50 +0200

    finished first lab (created spring boot project)

---
+++ libraryLabsProject/src/main/java/priv/sin/librarylabsproject/LibraryLabsProjectApplication.java
@@ -0,0 +1,13 @@
+package priv.sin.librarylabsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLabsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLabsProjectApplication.class, args);
+	}
+
+}
---
+++ libraryLabsProject/src/test/java/priv/sin/librarylabsproject/LibraryLabsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.sin.librarylabsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLabsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
