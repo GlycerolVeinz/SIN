747a1470f9a474dd0129191ec950d923a2de3665
GlycerolVeinz <safrankou.matvei@gmail.com>
2024-11-12 09:22:42 +0100

    added graceful shutdown

--- libraryLabsProject/src/main/java/priv/sin/librarylabsproject/LibraryLabsProjectApplication.java
+++ libraryLabsProject/src/main/java/priv/sin/librarylabsproject/LibraryLabsProjectApplication.java
@@ -2,12 +2,34 @@ package priv.sin.librarylabsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class LibraryLabsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryLabsProjectApplication.class, args);
-	}
+		SpringApplication app = new SpringApplication(LibraryLabsProjectApplication.class);
+		ApplicationContext context = app.run(args);
 
+
+		// Start a separate thread to listen for the Esc key
+		new Thread(() -> {
+			System.out.println("Type 'zero' key to exit the application...");
+			try {
+				while (true) {
+					int key = System.in.read();
+					// ASCII code for ESC is 27
+					if (key == 48) {
+						System.out.println("Shutting down...");
+						SpringApplication.exit(context);
+						System.exit(0);
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
 }
